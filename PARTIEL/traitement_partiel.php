a73dd257b8b080805db3e6b0eb41f5d3abb3eb45
arlaroussi <rli.laroussi@gmail.com>
2021-02-03 22:26:33 +0100

    Nouvelle branche développement

--- PARTIEL/traitement_partiel.php
+++ PARTIEL/traitement_partiel.php
@@ -2,11 +2,11 @@
 
 require "classes_partiel.php";
 
-$lot = array(new Lot(25000005, "Asus", 400, 100, 20),
-			 new Lot(350050, "Samsung", 100, 50, 10));
+$lot = array(new Lot(15000, "Asus", 400, 100, 20),
+			 new Lot(25000, "Samsung", 100, 50, 10));
 
 
-			 echo "<table border='1'";
+			 echo "<table border='2'";
 
 foreach ($lot as $elem) {
 	echo "<tr>";
