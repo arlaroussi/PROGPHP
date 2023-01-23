15fe4ac3501c00714e32809fa60653dec0a59154
Reda LAROUSSI <rl.laroussi@gmail.com>
2023-01-23 17:34:36 +0100

    Validation des branches

--- PARTIEL/Definition_Classes.php
+++ PARTIEL/Definition_Classes.php
@@ -51,12 +51,7 @@ class Personne {
 
 	//Méthodes
 	private function afficher() {
-		$this->num . '<br/>';
-		$this->nom . '<br/>';
-		$this->prenom . '<br/>';
-		$this->daten . '<br/>';
-		$this->adresse . '<br/>';
-		$this->entreprise->nom . '<br/>';
+
 	}
 
 	public calculerSalaire() {
--- PARTIEL/traitement.php
+++ PARTIEL/traitement.php
@@ -2,12 +2,9 @@
 
 require "ressources.php";
 
-$developpeur = array(new Developpeur(550,"Le Flock", "Louis-Marie", "lmleflock@test.net", "0606060606",2500, "Java"),
-					 new Developpeur(650,"Chellal", "Amine", "ach@test.net", "0707070707",2750, "C++")) ;
-
-
-$manager = array(new Manager(500,"RIPAULT", "Alexis", "alexis@manager.net", "0606060606",4000, "DSI"),
-					 new Manager(600,"OLIVEIRA", "Romain", "romain@manager.net", "0808080808",3000, "Chef de service")) ;
+$developpeur = array() ;
+ 
+$manager = array() ;
 
 
 foreach ($developpeur as $value) {
