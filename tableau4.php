9158c271df3ee80e6cb32268d9c5be92b00ef3d8
Reda LAROUSSI <rl.laroussi@gmail.com>
2023-01-25 09:26:44 +0100

    test

--- tableau4.php
+++ tableau4.php
@@ -1,8 +1,12 @@
 <?php
 
+<<<<<<< HEAD
 /* Ceci est un test */
 
 $marqueTab1 = array("volvo","BMW","Toyota");
+=======
+$marqueTab = array("volvo","BMW","Toyota");
+>>>>>>> parent of 3442ef7 (Tableau 4)
 
 for ($i=0; $i<count($marqueTab1); $i=++;)
 {
